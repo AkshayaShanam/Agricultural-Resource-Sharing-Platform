90bba4fca3da8d823bbba4e456666d912dc75d5d
AkshayaShanam <akshayashanam0123@gmail.com>
2025-05-20 12:02:33 +0530

    Removed old frontend and backend folders

--- backend/routes/profileRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const profileController = require('../controllers/profileController');
-const verifyToken = require('../middleware/verifyToken'); // using verifyToken instead of authenticate
-
-router.get('/', verifyToken, profileController.getProfile);
-router.put('/edit', verifyToken, profileController.updateProfile);
-router.get('/:id', verifyToken, profileController.getProfileById); // For fetching any user's profile
-module.exports = router;
--- backend/routes/searchRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { searchResources } = require('../controllers/searchController');
-const verifyToken = require('../middleware/verifyToken'); // if needed
-
-router.get('/resources', verifyToken, searchResources);
-
-module.exports = router;
